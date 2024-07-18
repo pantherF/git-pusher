62386768dd008b0501b67b5ad73d8ccc932781ae
black panther <94008173+pantherF@users.noreply.github.com>
2024-07-18 12:50:14 +0200

    better code and asdf

--- git-helper.sh
+++ git-helper.sh
@@ -1,62 +1,79 @@
 #!/bin/bash
 
+usage() {
+    echo "Example usage: $0 -p <project_path> -m [<commit_message>] -b [<branch>]"
+    echo "  -p <project_path>     : Path to the application directory (required)"
+    echo "  -m <commit_message>   : Commit message (default: random string of words)"
+    echo "  -b <branch>           : Branch to checkout (default: main)"
+    exit 1
+}
+
+APP_DIR=""
+COMMIT_MESSAGE=""
+BRANCH_NAME="main" # Default branch name
+
+while getopts ":p:m:b:" opt; do
+    case ${opt} in
+        p)
+            APP_DIR=$OPTARG
+            ;;
+        m)
+            COMMIT_MESSAGE=$OPTARG
+            ;;
+        b)
+            BRANCH_NAME=$OPTARG
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            usage
+            ;;
+        :)
+            echo "Option -$OPTARG requires an argument." >&2
+            usage
+            ;;
+    esac
+done
+
+# Check required arguments
+if [[ -z "$APP_DIR" ]]; then
+    echo "ERROR: Project path is required."
+    usage
+fi
+
 function random_words() {
     random=$(shuf -i 1-10 -n 1)
-
     response=$(curl -s "https://random-word-api.herokuapp.com/word?number=$random")
 
     if [ $? -ne 0 ]; then
         echo "ERROR: Failed to fetch data from the API."
         exit 1
-    fi
+    fi  
 
     if [ -z "$response" ]; then
         echo "ERROR: Empty response from the API."
         exit 1
-    fi
+    fi  
 
     sentence=$(echo "$response" | jq -r '.[]' | tr '\n' ' ' | sed 's/ $//')
 
     if [ $? -ne 0 ]; then
         echo "ERROR: Failed to parse JSON."
         exit 1
-    fi
+    fi  
 
     echo $sentence
 }
 
-RAND_NUM=$(shuf -i 5-10 -n 1)
-
-if [ -z "$1" ]; then
-    echo "ERROR: No project path provided."
-    echo "Usage: $0 [project path] [commit-message] [branch]"
-    exit 1
-elif [ -n "$1" ]; then
-  PROJECT_PATH="$1"
-fi
-
-if [ -z "$2" ]; then
-    echo "WARNING: No commit message provided."
-    echo "Usage: $0 [project path] [commit-message] [branch]"
-    echo "INFO: generating random commit message."
+# Set commit message to random words if not provided
+if [[ -z "$COMMIT_MESSAGE" ]]; then
     COMMIT_MESSAGE=$(random_words)
-elif [ -n "$2" ]; then
-  COMMIT_MESSAGE="$2"
 fi
 
-if [ -z "$3" ]; then
-    echo "WARNING: No branch name provided."
-    echo "Usage: $0 [project path] [commit-message] [branch]"
-    echo "INFO: setting 'main' as the default branch."
-    BRANCH_NAME="main"
-elif [ -n "$3" ]; then
-  BRANCH_NAME="$3"
-fi
-
-cd $PROJECT_PATH
+cd $APP_DIR
 
 git add .
 git commit -m "$COMMIT_MESSAGE"
-git push -u origin $BRANCH_NAME
+git push -u origin "$BRANCH_NAME"
 
 echo "Execution complete."
+
